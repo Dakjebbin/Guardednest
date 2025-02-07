0c70122a89401197f878f927b78d538666ed2eba
DakJebbin <Jebbinp@gmail.com>
2025-02-07 23:33:59 +0100

    iu updates

--- frontend/src/User/Settings.jsx
+++ frontend/src/User/Settings.jsx
@@ -122,42 +122,47 @@ axios.defaults.withCredentials = true
               </div>
           </div>
           <div className="sector">
-            <div className="verify">
-              <Link>
-                <svg
+        
+ 
+            <div className="border border-[#cb8d39] mb-4 py-8 px-1">
+              <Link className="flex justify-center items-center">
+              <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 640 512"
-                  width={"63px"}
+                  width={"50px"}
+                  className="bg-[#cb8d39] px-2 py-2 rounded-lg"
                 >
                   <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c1.8 0 3.5-.2 5.3-.5c-76.3-55.1-99.8-141-103.1-200.2c-16.1-4.8-33.1-7.3-50.7-7.3H178.3zm308.8-78.3l-120 48C358 277.4 352 286.2 352 296c0 63.3 25.9 168.8 134.8 214.2c5.9 2.5 12.6 2.5 18.5 0C614.1 464.8 640 359.3 640 296c0-9.8-6-18.6-15.1-22.3l-120-48c-5.7-2.3-12.1-2.3-17.8 0zM591.4 312c-3.9 50.7-27.2 116.7-95.4 149.7V273.8L591.4 312z" />
                 </svg>
-                <p>Verifications</p>
+                <p className="text-3xl">Verifications  </p>
               </Link>
             </div>
-
-            <div className="emailo">
-              <Link>
-                <svg
+            
+            <div className="border border-[#cb8d39] mb-4 py-8 px-1">
+              <Link className="flex justify-center items-center">
+              <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 512 512"
-                  width={"59px"}
+                  width={"50px"}
+                   className="bg-[#cb8d39] px-2 py-2 rounded-lg"
                 >
                   <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                 </svg>
-                <p>Update Email</p>
+                <p className="text-3xl">Update Email</p>
               </Link>
             </div>
-
-            <div className="password">
-              <Link>
+            
+            <div className="border border-[#cb8d39] py-8 px-1">
+              <Link className="flex justify-center items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
-                  width={"55px"}
+                  width={"40px"}
+                  className="bg-[#cb8d39] px-2 py-2 rounded-lg"
                 >
                   <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" />
                 </svg>
-                <p>Update Password</p>
+                <p className="text-3xl">Update Password</p>
               </Link>
             </div>
           </div>
